a032759d168e8f7491ea157ba4da29b8d7a9494d
Philipp Hinrichsen <philipp@sp3c1.com>
2025-09-09 19:35:31 +0200

    Allow upper-case in header-case rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -22,6 +22,6 @@ export default {
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
     "header-max-length": [2, "always", 100],
-    "header-case": [2, "always", ["camel-case", "lower-case"]],
+    "header-case": [2, "always", ["camel-case", "lower-case", "upper-case"]],
   },
 };
