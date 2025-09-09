78e06d1d84b46c2bec83a64c031d2fc36bb40039
Poltergeist <philipp@sp3c1.com>
2025-09-09 19:42:32 +0200

    chore: adjust commitlint config to support kebab-case

--- commitlint.config.js
+++ commitlint.config.js
@@ -18,10 +18,13 @@ export default {
         "test",
       ],
     ],
-    "subject-case": [2, "always", "lower-case"],
+    "subject-case": [
+      2,
+      "always",
+      ["camel-case", "lower-case", "upper-case", "kebab-case"],
+    ],
     "subject-empty": [2, "never"],
     "subject-full-stop": [2, "never", "."],
     "header-max-length": [2, "always", 100],
-    "header-case": [2, "always", ["camel-case", "lower-case", "upper-case"]],
   },
 };
